92f35487aaeb2a9a0551ffab47fa3ff614cf5770
Emilly Arcanjo <emilly.ssa@gmail.com>
2022-11-14 15:36:54 -0300

    js do bloqueio do login

---
+++ Projeto_MEAU/static/js/login.js
@@ -0,0 +1,39 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var user = document.getElementById("username").value;
+    var senha = document.getElementById("password").value;
+
+    if (user === "operador" && senha === "operador123") {
+        window.location.assign ("../crud");
+        return false;
+    }
+
+    else if (user === "funcionario" && senha === "funcionario123") {
+        window.location.assign ("../monitoring");
+        return false;
+    }
+
+    else if (user === "gerente" && senha === "gerenter123") {
+        window.location.assign ("../reports");
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("Você ainda tem "+attempt+" tentativas.");
+        // Disabling fields after 3 attempts.
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
+function blockLogin(){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
+    return false;
+}
